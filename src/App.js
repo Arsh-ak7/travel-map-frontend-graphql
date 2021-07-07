92d64432021e0438820d94fe467fe1c4a46ef333
Arsh-ak7 <arshkr07@gmail.com>
2021-07-07 17:56:03 +0530

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { AuthProvider } from "./Context/auth";
+import Home from "./Pages/Home";
+import Navbar from "./Components/Navbar";
+import AuthRoute from "./Utils/authRoute";
 
 function App() {
-	return <div className='app'></div>;
+	return (
+		<div className='app'>
+			<AuthProvider>
+				<Router>
+					<Navbar />
+				</Router>
+			</AuthProvider>
+		</div>
+	);
 }
 
 export default App;
